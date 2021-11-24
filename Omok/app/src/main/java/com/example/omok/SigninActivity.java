8e4a30c659d2acc6bba443971adb657fbd4d54e2
rlatp1409 <stanley710@naver.com>
2021-11-25 03:21:30 +0900

    회원가입 xml 제작, 파이어베이스 연동, SecondActivity 까지 화면 이동 구현

--- Omok/app/src/main/java/com/example/omok/SigninActivity.java
+++ Omok/app/src/main/java/com/example/omok/SigninActivity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SecondActivity extends AppCompatActivity {
+public class SigninActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_main);
+        setContentView(R.layout.activity_signin);
     }
 }
